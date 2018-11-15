7a0a6e103a5d64400b6b82d206438009e7d4dcdd
ken-chan <daniil.mahotkin@mail.ru>
2018-11-15 21:19:20 +0300

    figth_fly

--- ai_controls.py
+++ ai_controls.py
@@ -1,8 +1,9 @@
 from multiprocessing import Process
-from aaaaaaa.messages import *
+from messages import *
 import pyglet
-import aaaaaaa.messages as messages
-from aaaaaaa.obj_def import *
+import math
+import messages
+from obj_def import *
 from random import *
 from pyglet.window import key as pygletkey
 
@@ -16,6 +17,7 @@ class AIcontrolsState:
 class AIcontrols(Process):
     def __init__(self, messenger):
         super(AIcontrols, self).__init__()
+        self.size = 1000
         self.ai_state = AIcontrolsState.Start
         self.messenger = messenger
         self.objects_copy = None
@@ -53,10 +55,38 @@ class AIcontrols(Process):
                     x = self.objects_copy[index][ObjectProp.Xcoord]
                     y = self.objects_copy[index][ObjectProp.Ycoord]
                     dir = self.objects_copy[index][ObjectProp.Dir]
-                    x = np.array([[x / 1000, y / 1000, dir / 360],[x / 1000, y / 1000, dir / 360]], np.float32)
-                    X = x[0:1, 0:3]
+                    radius = math.sqrt(abs((x - self.size/2)*(x - self.size/2))+abs((y - self.size/2)*(y - self.size/2)))
+                    res = 0
 
-                    predictions = model.predict(X)
-                    res = (predictions[0][0])
-                    #print(res)
+                    xe = self.objects_copy[0][ObjectProp.Xcoord]
+                    ye = self.objects_copy[0][ObjectProp.Ycoord]
+                    dire = self.objects_copy[0][ObjectProp.Dir]
+                    """tmp = np.array([[x / 1000, y / 1000, dir / 360, xe/1000, ye/1000],[x / 1000, y / 1000, dir / 360, xe/1000, ye/1000]], np.float32)
+                    X = tmp[0:1, 0:5]"""
+                    if(radius > self.size/2-150):
+                        tmp = np.array([[x / self.size, y / self.size, dir / 360],
+                                        [x / self.size, y / self.size, dir / 360]], np.float32)
+                        X = tmp[0:1, 0:3]
+                        predictions = model.predict(X)
+                        res = (predictions[0][0])
+                        print(tmp[0] ," res=",res)
+                    else:
+                        dest_x = xe - 40 * math.sin(math.radians(dire))
+                        dest_y = ye - 40 * math.cos(math.radians(dire))
+
+                        rel_x = dest_x - x
+                        rel_y = dest_y - y
+                        betta = 0
+                        if rel_x > 0 and rel_y > 0:
+                            betta = math.degrees(math.atan((abs(rel_x)) / (abs(rel_y) + 0.01)))
+                        elif rel_x > 0 and rel_y < 0:
+                            betta = 180 - math.degrees(math.atan((abs(rel_x)) / (abs(rel_y) + 0.01)))
+                        elif rel_x < 0 and rel_y < 0:
+                            betta = 180 + math.degrees(math.atan((abs(rel_x)) / (abs(rel_y) + 0.01)))
+                        elif rel_x < 0 and rel_y > 0:
+                            betta = 360 - math.degrees(math.atan((abs(rel_x)) / (abs(rel_y) + 0.01)))
+                        print(betta, " ",dir)
+                        if(dir < betta and abs(dir - betta) < 180 or dir > 270 and betta < 180 - (360 - dir) or
+                                betta > 270 and dir > 180 - (360 - betta) and dir < betta):
+                            res = 1
                     self.messenger.bot2_set_pressed_key(pressed, round(res))
